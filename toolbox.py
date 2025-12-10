4108a5e2f9072f22a5f7a09781c2cce7f682f4b8
Tom Thorsén <tomthorsen@live.se>
2025-12-10 06:37:23 +0100

    Börjat med menyn

---
+++ toolbox.py
@@ -0,0 +1,76 @@
+# Detta blir basen i mitt projekt, själva menyn där man väljer vilket verktyg man vill använda.
+
+#!/usr/bin/env python3
+
+import os
+import sys
+import time
+from tools import net_tools
+
+# --- Funktion för att skriva ett tecken i taget ---
+
+def typer(text, delay=0.1):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print()
+
+# --- CLEAR SCREEN FUNCTION ---
+
+def clrscr():
+    #WIN ELLER LINUX
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+# --- MENY-SYSTEMET ---
+
+def menu() -> None:
+    print(r"""
+          
+    ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+    ▐                                                      ▌
+    ▐   _______ ______ ______ ____   __  __ ____ _______   ▌
+    ▐  |_     _|      |      |_   | |  |/  |_   |_     _|  ▌
+    ▐    |   | |  --  |  --  |_|  |_|     < _|  |_|   |    ▌
+    ▐    |___| |______|______|______|__|\__|______|___|    ▌
+    ▐                                                      ▌
+    ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+          
+                  CYBER SECURITY TOOLKIT
+                         MAIN MENU
+           """)
+    typer("1. Portscanner")
+    typer("2. Placeholder" )
+    typer("3. Placeholder" )
+    typer("4. Placeholder" )
+    typer("[0] Exit")    
+
+def main():
+    while True:
+        clrscr()  # --- RENSA SKÄRMEN --- 
+        menu()
+        val = input("Ch00s3 yu0r t001: ")
+
+        if val == "2":
+            #portscanner()
+            pass
+        elif val == "2":
+            #Placeholder
+            pass
+        elif val == "3":
+            #Placeholder
+            pass
+        elif val == "0":
+            typer("\n --- EXITING ---")
+            time.sleep(1)
+            sys.exit()
+        else:
+            typer("Wr0ng 1nput, Try Ag61n: ")
+
+
+
+
+main()
